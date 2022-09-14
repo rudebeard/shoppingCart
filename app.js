3fd7909d5deb1f9f9f71191b226c3352e09dc4fe
rudebeard <ademkoroglu542@gmail.com>
2022-09-14 18:42:39 -0400

    buttons are working

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-import { shopItemsData } from "./list.js"; 
 
-console.log(shopItemsData)
 const shop = document.querySelector('#shop');
 
+const shopItemsData = [
+  {
+  id:'aga',
+  name:'casual shirt',
+  price:45,
+  desc:"Lorem ipsum dolor sit amet.",
+  img:"./images/img-1.jpg"
+},
+
+{ id:'sen',
+name:'Official',
+price:100,
+desc:"Lorem ipsum dolor sit amet.",
+img:"./images/img-2.jpg"},
+
+{ id:'nereden',
+name:'Daily shirt',
+price:85,
+desc:"Lorem ipsum dolor sit amet.",
+img:"./images/img-3.jpg"},
+
+{
+  id:'ciktin',
+  name:'Mans suit',
+  price:300,
+  desc:"Lorem ipsum dolor sit amet.",
+  img:"./images/img-4.jpg"
+}
+];
 
 
-
+let basket= [];
 
 let generateShop = ()=>{
   return (shop.innerHTML= shopItemsData.map((x)=>{
     let {id,name,price,desc,img} = x;
-    console.log(x)
     return `
     <div id="${id}"  class="item">
     <img width="250" src=${img} alt="">
@@ -20,9 +46,9 @@ let generateShop = ()=>{
       <div class="price-quantity">
         <h2> $ ${price}</h2>
         <div class="buttons">
-          <i class="bi bi-plus"></i>
+          <i onclick="increment(${id})" class="bi bi-plus"></i>
           <div id = "${id}" class="quantity">0</div>
-          <i class="bi bi-dash"></i>
+          <i onclick="decrement(${id})" class="bi bi-dash"></i>
         </div>
       </div>
     </div>
@@ -30,3 +56,43 @@ let generateShop = ()=>{
   }).join(""));
 }
 generateShop();
+
+let increment = (id)=>{
+  let selectedItem= id;
+  let sera = basket.find((x)=>x.id===selectedItem.id);
+  if(sera===undefined){
+    basket.push({
+      id:selectedItem.id,
+      item:1,
+    });
+  }else {
+    sera.item++;
+  }
+  upgrade()
+}
+
+let decrement = (id) =>{
+  let selectedItem= id;
+  let sera = basket.find((x)=>x.id===selectedItem.id);
+
+  if(sera.item===0) return;
+   else {
+    sera.item--;
+  } 
+  upgrade()
+}
+
+let upgrade = ()=>{
+  console.log('upgrade function is working')
+}
+
+
+
+
+
+
+
+
+
+
+
